15b0e7ec27bdd052c5c102b24ad923cb2a211759
Derric samson <140136414+Derric01@users.noreply.github.com>
2025-05-07 23:07:56 +0530

    commit

---
+++ src/app/about/page.js
@@ -0,0 +1,8 @@
+export default function AboutPage(){
+    return(
+        <main className="p-10">
+            <h1 className="text-3xl font-bold">About us</h1>
+            <p className="mt-4 text-lg">This is the about page.</p>
+        </main>
+    )
+}
---
+++ src/app/contact/page.js
@@ -0,0 +1,8 @@
+export default function ContactPage(){
+    return(
+        <main className="p-10">
+            <h1 className="text-3xl font-bold">Contact us</h1>
+            <p className="mt-4 text-lg">Get in touch with us in here</p>
+        </main>
+    )
+}
